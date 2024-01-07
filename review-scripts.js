942697536edeafbce9717ade5599d35ee192bc47
Payod31 <payodshendre12345@gmail.com>
2024-01-08 02:54:40 +0530

    s

--- review-scripts.js
+++ review-scripts.js
@@ -18,10 +18,32 @@ function submitReview(event) {
     // Display a thank you message
     alert('Thank you for your review!');
 
+    // Create a new review object
+    var review = {
+        name: nameInput.value,
+        rating: ratingSelect.value,
+        comment: commentTextarea.value
+    };
+
+    // Save the review to local storage
+    saveReview(review);
+
     // Redirect to subscription.html after 2 seconds
     setTimeout(function () {
-        window.location.href = 'index.html';
+        window.location.href = 'subscription.html';
     }, 2000);
+}
+
+// Function to save a review to local storage
+function saveReview(review) {
+    // Retrieve existing reviews from local storage
+    var reviews = JSON.parse(localStorage.getItem('reviews')) || [];
+
+    // Add the new review to the list
+    reviews.push(review);
+
+    // Save the updated reviews list to local storage
+    localStorage.setItem('reviews', JSON.stringify(reviews));
 }
 
 // Event listener for form submission
