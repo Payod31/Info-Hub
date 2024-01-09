b2e7e2fd6e13ad2cf60b472f5e3acd9d0b2a4df3
Payod31 <payodshendre12345@gmail.com>
2024-01-10 01:34:36 +0530

    m

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function showMockResults(query) {
   clearResults();
 
   // Display search results based on the query (case-insensitive)
+  var lowerCaseQuery = query.toLowerCase(); 
   switch (query.toLowerCase()) {
       case 'data science':
           showResults('videoResults', searchData.dataScience.videos);
@@ -19,7 +20,7 @@ function showMockResults(query) {
           showResults('articleResults', searchData.dataScience.articles);
           showResults('blogResults', searchData.dataScience.blogs);
           showResults('studyMaterialResults', searchData.dataScience.studyMaterial);
-          showResults('photoResults', searchData.dataScience.photos);
+          showResults('WebsiteResults', searchData.dataScience.Website);
           showResults('courseResults', searchData.dataScience.courses);
           showResults('interviewResults', searchData.dataScience.interviewQuestions);
           showResults('qnaResults', searchData.dataScience.qna);
@@ -30,11 +31,23 @@ function showMockResults(query) {
           showResults('articleResults', searchData.marketing.articles);
           showResults('blogResults', searchData.marketing.blogs);
           showResults('studyMaterialResults', searchData.marketing.studyMaterial);
-          showResults('photoResults', searchData.marketing.photos);
+          showResults('WebsiteResults', searchData.marketing.Website);
           showResults('courseResults', searchData.marketing.courses);
           showResults('interviewResults', searchData.marketing.interviewQuestions);
           showResults('qnaResults', searchData.marketing.qna);
           break;
+      case 'digital marketing':
+            showResults('videoResults', searchData.digitalMarketing.videos);
+            showResults('bookResults', searchData.digitalMarketing.books);
+            showResults('articleResults', searchData.digitalMarketing.articles);
+            showResults('blogResults', searchData.digitalMarketing.blogs);
+            showResults('studyMaterialResults', searchData.digitalMarketing.studyMaterial);
+            showResults('WebsiteResults', searchData.digitalMarketing.Website);
+            showResults('courseResults', searchData.digitalMarketing.courses);
+            showResults('interviewResults', searchData.digitalMarketing.interviewQuestions);
+            showResults('qnaResults', searchData.digitalMarketing.qna);
+            break;
+
       // Add more cases for other topics as needed
       default:
           console.error('Invalid search query. Please use a supported topic.');
@@ -89,40 +102,40 @@ var searchData = {
           // Add more blog results as needed
       ],
       studyMaterial: [
-          { title: 'Data Science Learning Path', url: '#' },
-          { title: 'Fundamentals of Data Analysis', url: '#' },
+          { title: 'Recommended StudyMaterial 1', url: 'https://www.datacamp.com/courses/understanding-data-science' },
+          { title: 'Recommended StudyMaterial 2', url: 'https://playground.tensorflow.org/#activation=tanh&batchSize=10&dataset=circle&regDataset=reg-plane&learningRate=0.03&regularizationRate=0&noise=0&networkShape=4,2&seed=0.20530&showTestData=false&discretize=false&percTrainData=50&x=true&y=true&xTimesY=false&xSquared=false&ySquared=false&cosX=false&sinX=false&cosY=false&sinY=false&collectStats=false&problem=classification&initZero=false&hideText=false' },
           // Add more study material results as needed
       ],
-      photos: [
-          { title: 'Data Science Conference Photos', url: '#' },
-          { title: 'Visualizing Data Concepts', url: '#' },
-          // Add more photo results as needed
+      Website: [
+          { title: 'Recommended Website 1', url: 'https://www.w3schools.com/datascience/' },
+          { title: 'Recommended Website 2', url: 'https://www.simplilearn.com/tutorials/data-science-tutorial/what-is-data-science' },
+          // Add more website results as needed
       ],
       courses: [
-          { title: 'Mastering Data Science', url: '#' },
-          { title: 'Data Science Certification Course', url: '#' },
+          { title: 'Recommended Courses 1', url: 'https://www.coursera.org/learn/python-data-analysis' },
+          { title: 'Recommended Courses 2', url: 'https://www.codecademy.com/catalog/language/python?g_network=g&g_productchannel=&g_adid=624888211335&g_locinterest=&g_keyword=codecademy%20data%20engineering&g_acctid=243-039-7011&g_adtype=&g_keywordid=kwd-1184748332520&g_ifcreative=&g_campaign=account&g_locphysical=9062241&g_adgroupid=128133971748&g_productid=&g_source={sourceid}&g_merchantid=&g_placement=&g_partition=&g_campaignid=12575778360&g_ifproduct=&utm_id=t_kwd-1184748332520:ag_128133971748:cp_12575778360:n_g:d_c&utm_source=google&utm_medium=paid-search&utm_term=codecademy%20data%20engineering&utm_campaign=INTL_Brand_Phrase&utm_content=624888211335&g_adtype=search&g_acctid=243-039-7011&gclid=CjwKCAiA-vOsBhAAEiwAIWR0TaIz1zd74HTHY6uekPufotBaT9Au0UAne1jNgHqcvwAm9UAaCUGUehoCPvcQAvD_BwE' },
           // Add more course results as needed
       ],
       interviewQuestions: [
-          { title: 'Common Data Science Interview Questions', url: '#' },
-          { title: 'Preparing for a Data Science Interview', url: '#' },
+          { title: 'Recommended Interview Questions 1', url: 'https://www.glassdoor.com/Interview/DataCamp-Interview-Questions-E1192540.htm' },
+          { title: 'Recommended Interview Questions 2', url: 'https://www.kaggle.com/discussions' },
           // Add more interview question results as needed
       ],
       qna: [
-          { title: 'Data Science Q&A Forum', url: '#' },
-          { title: 'Ask the Experts: Data Science Edition', url: '#' },
+          { title: 'Recommended QnA 1', url: 'https://www.quora.com/What-is-data-science' },
+          { title: 'Recommended QnA 2', url: 'https://stackoverflow.com/questions/tagged/data-science' },
           // Add more Q&A results as needed
       ],
   },
   marketing: {
       videos: [
-          { title: 'Introduction to Marketing', url: '#' },
-          { title: 'Marketing Strategies', url: '#' },
+          { title: 'Recommended Videos 1', url: 'https://www.youtube.com/user/GaryVaynerchuk' },
+          { title: 'Recommended Videos 2', url: 'https://www.youtube.com/watch?v=b8oP6oeJ5WM' },
           // Add more video results as needed
       ],
       books: [
-          { title: 'Marketing Basics', url: '#' },
-          { title: 'Digital Marketing Guide', url: '#' },
+          { title: 'Recommended Books 1', url: 'https://www.amazon.com/Marketing-Metrics-Definitive-Measuring-Performance/dp/0137058292' },
+          { title: 'Recommended Books 2', url: 'https://www.amazon.com/Everybody-Writes-Go-Creating-Ridiculously-ebook/dp/B00LMB5P0G' },
           // Add more book results as needed
       ],
       articles: [
@@ -140,10 +153,10 @@ var searchData = {
           { title: 'Fundamentals of Marketing Analysis', url: '#' },
           // Add more study material results as needed
       ],
-      photos: [
+      Website: [
           { title: 'Marketing Event Photos', url: '#' },
           { title: 'Visualizing Marketing Concepts', url: '#' },
-          // Add more photo results as needed
+          // Add more website results as needed
       ],
       courses: [
           { title: 'Mastering Marketing', url: '#' },
@@ -161,5 +174,52 @@ var searchData = {
           // Add more Q&A results as needed
       ],
   },
+  digitalMarketing: {
+    videos: [
+        { title: 'Introduction to Digital Marketing', url: '#' },
+        { title: 'Digital Marketing Strategies', url: '#' },
+        // Add more video results as needed
+    ],
+    books: [
+        { title: 'Digital Marketing Basics', url: '#' },
+        { title: 'Advanced Digital Marketing Guide', url: '#' },
+        // Add more book results as needed
+    ],
+    articles: [
+        { title: 'Effective Digital Marketing Techniques', url: '#' },
+        { title: 'Trends in Digital Marketing', url: '#' },
+        // Add more article results as needed
+    ],
+    blogs: [
+        { title: 'Digital Marketing Insights Blog', url: '#' },
+        { title: 'Practical Digital Marketing Tips', url: '#' },
+        // Add more blog results as needed
+    ],
+    studyMaterial: [
+        { title: 'Digital Marketing Learning Path', url: '#' },
+        { title: 'Fundamentals of Digital Marketing Analysis', url: '#' },
+        // Add more study material results as needed
+    ],
+    Website: [
+        { title: 'Digital Marketing Event Photos', url: '#' },
+        { title: 'Visualizing Digital Marketing Concepts', url: '#' },
+        // Add more photo results as needed
+    ],
+    courses: [
+        { title: 'Mastering Digital Marketing', url: '#' },
+        { title: 'Digital Marketing Certification Course', url: '#' },
+        // Add more course results as needed
+    ],
+    interviewQuestions: [
+        { title: 'Common Digital Marketing Interview Questions', url: '#' },
+        { title: 'Preparing for a Digital Marketing Interview', url: '#' },
+        // Add more interview question results as needed
+    ],
+    qna: [
+        { title: 'Digital Marketing Q&A Forum', url: '#' },
+        { title: 'Ask the Experts: Digital Marketing Edition', url: '#' },
+        // Add more Q&A results as needed
+    ],
+},
   // Add more topics as needed
 };
